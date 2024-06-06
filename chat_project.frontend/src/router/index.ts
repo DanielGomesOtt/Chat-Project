018ef9991a74744be2f0d12463caa7aaeb62d364
danielott <danielgomesott@gmail.com>
2024-06-06 16:29:01 -0300

    começando a home

--- chat_project.frontend/src/router/index.ts
+++ chat_project.frontend/src/router/index.ts
@@ -1,10 +1,10 @@
-import { createRouter, createWebHistory } from 'vue-router'
-
+import { createRouter, createWebHistory } from 'vue-router';
+import Home from '../views/Home.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-   
+    {path:'/', component:Home},
   ]
 })
 
---
+++ chat_project.frontend/src/views/Home.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+    <div class="flex">
+        <div class="flex justify-between items-center h-screen w-screen">
+            <div class="ml-32">
+                <img src="../assets/img/computer.jpg" alt="notebook" class="w-96 rounded-full shadow-xl">
+            </div>
+            <div class="mr-32">
+                <div>
+                    <h1 class="text-black font-extrabold text-5xl">Converse com seus amigos</h1>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
