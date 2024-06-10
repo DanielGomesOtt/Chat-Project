82d1c702c0728b607ec16597d6895d0222d7995c
danielott <danielgomesott@gmail.com>
2024-06-10 09:18:22 -0300

    desenvolvendo a home

--- chat_project.frontend/src/views/Home.vue
+++ chat_project.frontend/src/views/Home.vue
@@ -3,15 +3,27 @@
 </script>
 
 <template>
-    <div class="flex">
-        <div class="flex justify-between items-center h-screen w-screen">
-            <div class="ml-32">
-                <img src="../assets/img/computer.jpg" alt="notebook" class="w-96 rounded-full shadow-xl">
+    <div>
+        <div class=" flex-wrap justify-center flex md:justify-around w-screen mt-24 md:mt-48">
+            <div class="p-2 md:mr-10 md:ml-32">
+                <div class="bg-blue-400 rounded-lg w-32 p-2 shadow">
+                    <span class="text-white text-xl">Chat Project</span>
+                </div>
+                <div class="mt-2">
+                    <h1 class="text-black font-bold text-xl w-[100%] md:text-5xl md:w-96">Converse com seus amigos</h1>
+                </div>
+                <div class="md:w-96 mt-5 md:text-xl text-justify text-slate-500">
+                    <p>Em tempo real ou a qualquer momento: o Chat ajuda os usuários a manter a conexão.</p>
+                </div>
+                <div>
+                    <button class="bg-blue-600 text-white rounded-md p-1 md:p-3 mt-5 shadow w-[100%] md:w-48 text-lg md:text-xl">Fazer Login</button>
+                </div>
+                <div class="flex md:justify-start justify-center">
+                    <button class="text-blue-600 border-0 p-3 mt-5 w-48 font-bold decoration-solid decoration-current decoration-2 underline">Criar conta</button>
+                </div>
             </div>
             <div class="mr-32">
-                <div>
-                    <h1 class="text-black font-extrabold text-5xl">Converse com seus amigos</h1>
-                </div>
+                <img src="../assets/img/computer.jpg" alt="notebook" class=" w-0 lg:w-96 md:rounded-full shadow-xl">
             </div>
         </div>
     </div>
